3bdabc41eafd8e9ab5c3b732691c048323bcc342
BluettDream <tianmh0625@qq.com>
2024-03-25 00:11:07 +0800

    update

--- service/image_service.py
+++ service/image_service.py
@@ -2,11 +2,18 @@ import cv2 as cv
 import numpy as np
 from flask import jsonify, Response
 from werkzeug.datastructures import FileStorage
+from matplotlib import pyplot as plt
 
 
 def match_template(match_img: FileStorage, template_img: FileStorage, threshold: float) -> Response:
-    tiny_img = cv.imdecode(np.frombuffer(match_img.read(), np.uint8), cv.COLOR_BGR2GRAY)
-    big_img = cv.imdecode(np.frombuffer(template_img.read(), np.uint8), cv.COLOR_BGR2GRAY)
+    tiny_img = cv.imdecode(np.frombuffer(match_img.read(), np.uint8), cv.COLOR_RGB2GRAY)
+    big_img = cv.imdecode(np.frombuffer(template_img.read(), np.uint8), cv.COLOR_RGB2GRAY)
     result = cv.matchTemplate(tiny_img, big_img, cv.TM_CCOEFF_NORMED)
     mn, ma, mn_loc, ma_loc = cv.minMaxLoc(result)
-    return jsonify(list(ma_loc))
+    cv.rectangle(big_img, ma_loc, (ma_loc[0] + tiny_img.shape[1], ma_loc[1] + tiny_img.shape[0]), 255, 2)
+    # cv.imshow('Result', big_img)
+    # cv.waitKey(0)
+    return jsonify({
+        'similarity': ma,
+        'location': ma_loc
+    })
