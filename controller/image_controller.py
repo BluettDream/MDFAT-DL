e15273f25c29b9e8170861130a125ee2d6acac8d
BluettDream <tianmh0625@qq.com>
2024-03-28 23:45:15 +0800

    update

--- controller/image_controller.py
+++ controller/image_controller.py
@@ -5,6 +5,5 @@ img = Blueprint('img', __name__)
 
 @img.route("/image/matchPoints", methods=["POST"])
 def process():
-    threshold = float(request.form['threshold'])
     tiny_img, big_img = request.files['matchImage.png'], request.files['templateImage.png']
-    return image_service.match_template(tiny_img, big_img, threshold)
+    return image_service.match_template(tiny_img, big_img)
--- service/image_service.py
+++ service/image_service.py
@@ -2,17 +2,13 @@ import cv2 as cv
 import numpy as np
 from flask import jsonify, Response
 from werkzeug.datastructures import FileStorage
-from matplotlib import pyplot as plt
 
 
-def match_template(match_img: FileStorage, template_img: FileStorage, threshold: float) -> Response:
+def match_template(match_img: FileStorage, template_img: FileStorage) -> Response:
     tiny_img = cv.imdecode(np.frombuffer(match_img.read(), np.uint8), cv.COLOR_RGB2GRAY)
     big_img = cv.imdecode(np.frombuffer(template_img.read(), np.uint8), cv.COLOR_RGB2GRAY)
     result = cv.matchTemplate(tiny_img, big_img, cv.TM_CCOEFF_NORMED)
     mn, ma, mn_loc, ma_loc = cv.minMaxLoc(result)
-    cv.rectangle(big_img, ma_loc, (ma_loc[0] + tiny_img.shape[1], ma_loc[1] + tiny_img.shape[0]), 255, 2)
-    # cv.imshow('Result', big_img)
-    # cv.waitKey(0)
     return jsonify({
         'similarity': ma,
         'location': ma_loc
